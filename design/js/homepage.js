e2529801c87b51eb29414eb15cc4cb25ee5615e1
Andrés Londoño <camilo742006@gmail.com>
2026-03-02 23:03:32 -0500

    Desktop/Mobile Finished

---
+++ design/js/homepage.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    const mobileMenu = document.getElementById("mobile_menu");
+    mobileMenu.style.display = "flex";
+}
+
+function closeMenu() {
+    const mobileMenu = document.getElementById("mobile_menu");
+    mobileMenu.style.display = "none";
+}
